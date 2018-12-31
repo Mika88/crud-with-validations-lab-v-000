86f02805e3ded2f3d8a60defc875cd198e834100
Mika88 <mikaspivak@gmail.com>
2018-12-31 02:05:31 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :songs
   root "songs#index"
-  delete '/songs', to: 'songs#destroy', as:'song_delete'
+  delete '/songs', to: 'songs#destroy', as:'song-delete'
 end
